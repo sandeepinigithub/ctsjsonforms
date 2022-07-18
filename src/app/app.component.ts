ae67295c290c27539bbd8b9816bb70cdc3a471be
Sandeep Kumar Shukla <sandeepini.2012@gmail.com>
2022-07-18 19:12:20 +0530

    Initial setup done for jsonform custom in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import uischemaAsset from '../assets/uischema.json';
+import schemaAsset from '../assets/schema.json';
+import dataAsset from './data';
+import { createAjv, rankWith } from '@jsonforms/core';
+import { angularMaterialRenderers } from '@jsonforms/angular-material';
+
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +14,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ctsjsonforms';
+  uischema = uischemaAsset;
+  schema = schemaAsset;
+  data = dataAsset;
+  ajv = createAjv({
+    schemaId: 'id',
+    allErrors: true
+  });
+  renderers = [
+    ...angularMaterialRenderers,
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { JsonFormsModule } from '@jsonforms/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    JsonFormsModule,
+    JsonFormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
